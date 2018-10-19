a2b877031d1beee54a7cce85116bd57f008dc418
Nick <Ecton.Nick@gmail.com>
2018-10-18 21:13:09 -0700

    heroku db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,52 @@
 
 
 
-# require 'csv'
-#
-# # csv_text = File.read(Rails.root.join('lib', 'seeds', 'companylist.csv'))
-# # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# # puts csv_text
-# #
-# array = []
-# result = []
-#
-# File.foreach(Rails.root.join('lib', 'seeds', 'companylist.csv')) do |f|
-#   array.push([f])
-# end
-#
-# array.each do |comp|
-#   result.push(comp[0].split("\,\\\""))
-# end
-# answer = []
-# result.each do |por|
-#   answer.push(por[0].split("\,\""))
-# end
-#
-# finalTick = []
-# finalCompanies = []
-#
-# answer.each do |tickers|
-#   tick = tickers[0].delete_prefix('"').delete_suffix('"')
-#   finalTick.push(tick)
-# end
-#
-# answer.each do |company_name|
-#   comp_name = "#{company_name[1]}".chomp('"')
-#   finalCompanies.push(comp_name)
-# end
-#
-# company_hash = Hash.new()
-#
-# finalTick.each_with_index do |tick, idx|
-#   company_hash[idx+1] = {"Symbol": finalTick[idx], "Company": finalCompanies[idx]}
-# end
-#
-# company_hash.each do |key, value|
-#   Asset.create(value)
-# end
+require 'csv'
 
-# PortfolioSnapshot.destroy_all
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'companylist.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# puts csv_text
 #
+array = []
+result = []
+
+File.foreach(Rails.root.join('lib', 'seeds', 'companylist.csv')) do |f|
+  array.push([f])
+end
+
+array.each do |comp|
+  result.push(comp[0].split("\,\\\""))
+end
+answer = []
+result.each do |por|
+  answer.push(por[0].split("\,\""))
+end
+
+finalTick = []
+finalCompanies = []
+
+answer.each do |tickers|
+  tick = tickers[0].delete_prefix('"').delete_suffix('"')
+  finalTick.push(tick)
+end
+
+answer.each do |company_name|
+  comp_name = "#{company_name[1]}".chomp('"')
+  finalCompanies.push(comp_name)
+end
+
+company_hash = Hash.new()
+
+finalTick.each_with_index do |tick, idx|
+  company_hash[idx+1] = {"Symbol": finalTick[idx], "Company": finalCompanies[idx]}
+end
+
+company_hash.each do |key, value|
+  Asset.create(value)
+end
+
+Portfolio.create(user_id: 3)
+
 prng = Random.new
 year = 2013
 month = 10
@@ -90,6 +90,6 @@ while year != 2018
     month = 3
     day = 0
   else
-    PortfolioSnapshot.create(date: "#{year}-#{tempmonth}-#{tempday}", value: value, portfolio_id: 16)
+    PortfolioSnapshot.create(date: "#{year}-#{tempmonth}-#{tempday}", value: value, portfolio_id: 3)
   end
 end
